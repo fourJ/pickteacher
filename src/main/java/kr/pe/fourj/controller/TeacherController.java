b59ad7eb8fd75c9c7ab538220123b8baf7c82dfb
do-oni <dxxneo@gmail.com>
2021-10-12 12:16:19 +0900

    [Merge] TeacherController

--- src/main/java/kr/pe/fourj/controller/TeacherController.java
+++ src/main/java/kr/pe/fourj/controller/TeacherController.java
@@ -2,7 +2,6 @@ package kr.pe.fourj.controller;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.pe.fourj.domain.Course;
