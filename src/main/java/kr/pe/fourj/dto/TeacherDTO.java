483385d536e6fdc4dac953edf8fd0a1927c537cb
do-oni <dxxneo@gmail.com>
2021-10-12 14:49:33 +0900

    [Fix] Merge Conflict

--- src/main/java/kr/pe/fourj/dto/TeacherDTO.java
+++ src/main/java/kr/pe/fourj/dto/TeacherDTO.java
@@ -33,10 +33,6 @@ public class TeacherDTO {
 	public static class Get {
 		private Long idx;
 		private String name;
-<<<<<<< HEAD
-		private Integer birth;
-=======
->>>>>>> 875b0097c0e45a70869806f055bd100b81369f8e
 		private Integer gender;
 		private String school;
 		private String major;
